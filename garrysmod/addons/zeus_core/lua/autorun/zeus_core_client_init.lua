ace64372a20c61e67b7f1e8a4256e920880c2a7d
Ethan Hill <hill.l.ethan6066@gmail.com>
2026-01-21 21:40:55 +0000

    feat: add zeus_core_client_init.lua to load client scripts on CLIENT
    Co-authored-by: Cosine <agent@cosine.sh>

---
+++ garrysmod/addons/zeus_core/lua/autorun/zeus_core_client_init.lua
@@ -0,0 +1,8 @@
+if CLIENT then
+    include("zeus_core/config/sh_regiments.lua")
+    include("zeus_core/config/sh_zeus_config.lua")
+    include("zeus_core/shared/sh_util.lua")
+    include("zeus_core/client/cl_name_prompt.lua")
+    include("zeus_core/client/cl_hud.lua")
+    include("zeus_core/client/cl_scoreboard.lua")
+end
