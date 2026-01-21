c2e999a0e1c9e134385f4019b813039e6f20ea35
Ethan Hill <hill.l.ethan6066@gmail.com>
2026-01-21 22:50:24 +0000

    feat: include sh_sam_commands in zeus_core client init
    Co-authored-by: Cosine <agent@cosine.sh>

--- garrysmod/addons/zeus_core/lua/autorun/zeus_core_client_init.lua
+++ garrysmod/addons/zeus_core/lua/autorun/zeus_core_client_init.lua
@@ -2,6 +2,7 @@ if CLIENT then
     include("zeus_core/config/sh_regiments.lua")
     include("zeus_core/config/sh_zeus_config.lua")
     include("zeus_core/shared/sh_util.lua")
+    include("zeus_core/shared/sh_sam_commands.lua")
     include("zeus_core/client/cl_name_prompt.lua")
     include("zeus_core/client/cl_hud.lua")
     include("zeus_core/client/cl_scoreboard.lua")
